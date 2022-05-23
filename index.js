f28fc582ed6cdf8909b2ab7a31ad00b9a22c4c58
mdeveloper66 <m.developer66@gmail.com>
2022-05-23 05:41:52 +0430

    Light/Dark mode applied

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-// ---- Sidebar ----
+
 const sideMenu = document.querySelector('aside');
 const menuBtn = document.querySelector('#menu-btn');
 const closeBtn = document.querySelector('#close-btn');
+const themeToggler = document.querySelector('.theme-toggler');
 
+
+// ---- Open and close sidebar ----
 menuBtn.addEventListener('click', evt => {
     sideMenu.style.display = 'block';
 })
@@ -10,3 +13,12 @@ menuBtn.addEventListener('click', evt => {
 closeBtn.addEventListener('click', evt => {
     sideMenu.style.display = 'none';
 })
+
+// ---- Dark and light mode --------
+themeToggler.addEventListener('click', evt => {
+    document.body.classList.toggle('dark-theme-variables');
+
+    themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
+    themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
+})
+
